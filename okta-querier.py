14df5c4d583b55c801732dfb7e0e35b11c47b1c3
Ylan Muller <ymuller@firehydrant.com>
2024-05-24 15:15:29 -0700

    Significant formatting changes to make it more 'pythonic'. Also updated authentication method to move away from locally configured API key into OIDC.

--- okta-querier.py
+++ okta-querier.py
@@ -2,134 +2,163 @@
 # This utility lets you quickly pull up details about your Okta organization from a CLI interface
 
 import requests
+import webbrowser
+import time
 
-# Replace <Okta API Token> with a token from your environment
-headers = {"Authorization": "SSWS <Okta API Token>"}
-# Enter your custom domain with .okta.com at the end
-oktaDomain = ".okta.com"
+# Authorize against the OIDC app configured in your Okta tenant. Code courtesy of gabrielsokra: https://github.com/gabrielsroka
+# Set these values. See the README for more information:
+base_url = 'https://your_domain.okta.com'
+client_id = 'OIDC APP Client Id'
+scope = 'okta.users.read okta.groups.read okta.apps.read'
 
-def mainMenuChoice():
-    continueDecision = input("Would you like to return to the main menu? y/n: ")
-    if continueDecision == "y":
-        mainMenu()
-    elif continueDecision == "n":
+auth_url = base_url + '/oauth2/v1/device/authorize'
+auth_payload = {
+    'client_id': client_id,
+    'scope': 'openid ' + scope
+}
+auth = requests.post(auth_url, data=auth_payload).json()
+webbrowser.open_new(auth['verification_uri_complete'])
+
+while True:
+    time.sleep(2)
+    token_url = base_url + '/oauth2/v1/token'
+    token_payload = {
+        'client_id': client_id,
+        'device_code': auth['device_code'],
+        'grant_type': 'urn:ietf:params:oauth:grant-type:device_code'
+    }
+    tokens = requests.post(token_url, data=token_payload).json()
+    access_token = tokens.get('access_token')
+    if access_token: break
+
+session = requests.Session()
+session.headers['authorization'] = 'Bearer ' + access_token
+
+# Handling for whether to be returned to the main manu
+def main_menu_choice():
+    continue_decision = input('Would you like to return to the main menu? y/n: ')
+    if continue_decision == 'y':
+        main_menu()
+    elif continue_decision == 'n':
         exit
     else:
-        print("That is not a valid selection. Please try again.")
-        mainMenuChoice()
+        print('That is not a valid selection. Please try again.')
+        main_menu_choice()
 
-def getUserDetails():
-    # Lets you enter the email address of a user you want to look up, fetches the first match, and prints info
-    username = input("Enter the exact email address of an active user you want to look up: ")
-    findUserEndpoint = "https://" + oktaDomain + "/api/v1/users?search=profile.email Eq \"" + username + "\""
-    findUserDetails = requests.get(findUserEndpoint, headers=headers).json()
-    if findUserDetails:
-        userInfo = findUserDetails[0]
-        getUserGroupsEndpoint = "https://" + oktaDomain + f"/api/v1/users/{userInfo["id"]}/groups"
-        getUserGroups = requests.get(getUserGroupsEndpoint, headers=headers).json()
-        getUserAppsEndpoint = "https://" + oktaDomain + f"/api/v1/users/{userInfo["id"]}/appLinks"
-        getUserApps = requests.get(getUserAppsEndpoint, headers=headers).json()
+# Lets you enter the email address of a user you want to look up, fetches the first match, and prints info
+def get_user_details():
+    email = input('Enter the exact email address of an active user you want to look up: ')
+    find_user_endpoint = base_url + '/api/v1/users?search=profile.email Eq ' + '"' + email + '"'
+    find_user_details = session.get(find_user_endpoint).json()
+    if find_user_details:
+        user_info = find_user_details[0]
+        get_user_groups_endpoint = base_url + f'/api/v1/users/{user_info['id']}/groups'
+        get_user_groups = session.get(get_user_groups_endpoint).json()
+        get_user_apps_endpoint = base_url + f'/api/v1/users/{user_info['id']}/appLinks'
+        get_user_apps = session.get(get_user_apps_endpoint).json()
 
         # Modify this blob with the any fields from the profile that you want to include!
-        print(f"-----PROFILE DETAILS FOR {username}-----\n\nName: {userInfo["profile"]["firstName"]} {userInfo["profile"]["lastName"]}\nUser Status: {userInfo["status"]}\nDepartment: {userInfo["profile"]["department"]}\nManager Email: {userInfo["profile"]["managerEmail"]}\n\n")
+        print(f'-----PROFILE DETAILS FOR {email}-----\n\nName: {user_info['profile']['firstName']} {user_info['profile']['lastName']}\nUser Status: {user_info['status']}\nDepartment: {user_info['profile']['department']}\nManager Email: {user_info['profile']['managerEmail']}\n\n')
 
         # Prints all of the user's groups
-        userGroups = ""
-        for group in getUserGroups:
-            userGroups = userGroups + group["profile"]["name"] + "\n"
+        user_groups = ''
+        for group in get_user_groups:
+            user_groups = user_groups + group['profile']['name'] + '\n'
 
-        print(f"-----LIST OF USER'S GROUPS-----\n\n{userGroups}")
+        print(f'-----LIST OF USER\'S GROUPS-----\n\n{user_groups}')
 
         # Prints all of the user's assigned applications
-        userApps = ""
-        for app in getUserApps:
-            userApps = userApps + app["label"] + "\n"
+        user_apps = ''
+        for app in get_user_apps:
+            user_apps = user_apps + app['label'] + '\n'
         
-        print(f"-----LIST OF USER'S APPS-----\n\n{userApps}")
-        mainMenuChoice()
+        print(f'-----LIST OF USER\'S APPS-----\n\n{user_apps}')
+        main_menu_choice()
 
     else:
-        print("That user either does not exist in Okta or is inactive.")
-        mainMenuChoice()
+        print('That user either does not exist in Okta or is inactive.')
+        main_menu_choice()
 
-def getAppDetails():
-    # Lets you enter an app name, pull up a list of the applicable apps, enter the ID of the one you want to dig into, and then pull up details about it.
-    appName = input("Enter the name of the app you want to find: ")
-    findAppEndpoint = "https://" + oktaDomain + f"/api/v1/apps?q={appName}"
-    findApps = requests.get(findAppEndpoint, headers=headers).json()
+# Lets you enter an app name, pull up a list of the applicable apps, enter the ID of the one you want to dig into, and then pull up details about it.
+def get_app_details():
+    app_name = input('Enter the name of the app you want to find: ')
+    find_app_endpoint = base_url + f'/api/v1/apps?q={app_name}'
+    find_apps = session.get(find_app_endpoint).json()
 
-    if findApps:
-        appInfo = ""
-        for app in findApps:
-            appInfo = appInfo + f"Name: {app["label"]} | Id: {app["id"]}\n"
-        print(appInfo)
-        appId = input("Copy, paste, and submit the ID of the app you want more information on: ")
-        getAppDetailsEndpoint = "https://" + oktaDomain + f"/api/v1/apps/{appId}"
-        getAppDetails = requests.get(getAppDetailsEndpoint, headers=headers).json()
-        getAssociatedGroupsEndpoint = "https://" + oktaDomain + f"/api/v1/apps/{appId}/groups"
-        getAssociatedGroups = requests.get(getAssociatedGroupsEndpoint, headers=headers).json()
+    if find_apps:
+        app_info = ''
+        for app in find_apps:
+            app_info = app_info + f'Name: {app['label']} | Id: {app['id']}\n'
+        print(app_info)
+        app_id = input('Copy, paste, and submit the ID of the app you want more information on: ')
+        get_app_details_endpoint = base_url + f'/api/v1/apps/{app_id}'
+        get_app_details = session.get(get_app_details_endpoint).json()
+        get_associated_groups_endpoint = base_url + f'/api/v1/apps/{app_id}/groups'
+        get_associated_groups = session.get(get_associated_groups_endpoint).json()
 
         # Print some basic details about the application
-        print(f"-----APP DETAILS FOR {getAppDetails["label"]}-----\n\nName: {getAppDetails["name"]}\nStatus: {getAppDetails["status"]}\nSign-on Mode: {getAppDetails["signOnMode"]}\n\n")
+        print(f'-----APP DETAILS FOR {get_app_details['label']}-----\n\nName: {get_app_details['name']}\nStatus: {get_app_details['status']}\nSign-on Mode: {get_app_details['signOnMode']}\n\n')
 
         # Tidies up groups
         # Will also add the ability to query the ID of the group and pull the name into the line
-        groupList = ""
-        for group in getAssociatedGroups:
-            getGroupDetailsEndpoint = "https://" + oktaDomain + f"/api/v1/groups/{group["id"]}"
-            getGroupDetails = requests.get(getGroupDetailsEndpoint, headers=headers).json()
-            groupList = groupList + f"{getGroupDetails["profile"]["name"]} | {group["id"]} | Priority: {group["priority"]}\n"
+        group_list = ''
+        for group in get_associated_groups:
+            get_group_details_endpoint = base_url + f'/api/v1/groups/{group['id']}'
+            get_group_details = session.get(get_group_details_endpoint).json()
+            group_list = group_list + f'{get_group_details['profile']['name']} | {group['id']} | Priority: {group['priority']}\n'
 
-        print("-----ASSIGNED GROUPS-----\n\n" + groupList)
+        print('-----ASSIGNED GROUPS-----\n\n' + group_list)
+        main_menu_choice()
     else:
-        print("We were not able to find that app.")
-        mainMenuChoice()
+        print('We were not able to find that app.')
+        main_menu_choice()
 
-def getGroupDetails():
-    # Lets you enter the name of a group and returns a list of all potential matches
-    groupName = input("Enter the name of the group you want to find. HINT: Start from the beginning of the name: ")
-    findGroupEndpoint = "https://" + oktaDomain + f"/api/v1/groups?q={groupName}"
-    findGroups = requests.get(findGroupEndpoint, headers=headers).json()
+# Lets you enter the name of a group and returns a list of all potential matches
+def get_group_details():
+    group_name = input('Enter the name of the group you want to find. HINT: Start from the beginning of the name: ')
+    find_group_endpoint = base_url + f'/api/v1/groups?q={group_name}'
+    find_groups = session.get(find_group_endpoint).json()
 
-    if findGroups:
-        groupInfo = ""
-        for group in findGroups:
-            groupInfo = groupInfo + f"Name: {group["profile"]["name"]} | Id: {group["id"]}\n"
-        print(groupInfo)
-        groupId = input("Copy, paste, and submit the ID of the group you want more information on: ")
-        getGroupDetailsEndpoint = "https://" + oktaDomain + f"/api/v1/groups/{groupId}"
-        getGroupDetails = requests.get(getGroupDetailsEndpoint, headers=headers).json()
+    if find_groups:
+        group_info = ''
+        for group in find_groups:
+            group_info = group_info + f'Name: {group['profile']['name']} | Id: {group['id']}\n'
+        print(group_info)
+        group_id = input('Copy, paste, and submit the ID of the group you want more information on: ')
+        get_group_details_endpoint = base_url + f'/api/v1/groups/{group_id}'
+        get_group_details = session.get(get_group_details_endpoint).json()
 
         # Print details about the group
-        print(f"-----GROUP DETAILS FOR {getGroupDetails["profile"]["name"]}-----\n\nId: {getGroupDetails["id"]}\nDescription: {getGroupDetails["profile"]["description"]}\nCreated: {getGroupDetails["created"]}\nLast Membership Updated: {getGroupDetails["lastMembershipUpdated"]}\n\n")
+        print(f'-----GROUP DETAILS FOR {get_group_details['profile']['name']}-----\n\nId: {get_group_details['id']}\nDescription: {get_group_details['profile']['description']}\nCreated: {get_group_details['created']}\nLast Membership Updated: {get_group_details['lastMembershipUpdated']}\n\n')
 
         # Print all users
-        getAssociatedUsersEndpoint = "https://" + oktaDomain + f"/api/v1/groups/{groupId}/users"
-        getAssociatedUsers = requests.get(getAssociatedUsersEndpoint, headers=headers).json()
+        get_associated_users_endpoint = base_url + f'/api/v1/groups/{group_id}/users'
+        get_associated_users = session.get(get_associated_users_endpoint).json()
 
-        if getAssociatedUsers:
-            associatedUsers = ""
-            for user in getAssociatedUsers:
-                associatedUsers = associatedUsers + f"{user["profile"]["firstName"]} {user["profile"]["lastName"]} | Status: {user["status"]}\n"
-            print("-----MEMBERS-----\n\n" + associatedUsers)
+        if get_associated_users:
+            associated_users = ''
+            for user in get_associated_users:
+                associated_users = associated_users + f'{user['profile']['firstName']} {user['profile']['lastName']} | Status: {user['status']}\n'
+            print('-----MEMBERS-----\n\n' + associated_users)
+            main_menu_choice()
         else:
-            print("There are no users in this group.")
-            mainMenuChoice()
+            print('There are no users in this group.')
+            main_menu_choice()
     else:
-        print("We were not able to find a matching group.")
-        mainMenuChoice()
+        print('We were not able to find a matching group.')
+        main_menu_choice()
 
-def mainMenu():
-    print("What would you like to do today? Enter the number that corresponds to what you want to do from the list.\n1: Get user details\n2: Get app details\n3: Get group details")
-    choice = input("Make your selection: ")
-    if choice == "1":
-        getUserDetails()
-    elif choice == "2":
-        getAppDetails()
-    elif choice =="3":
-        getGroupDetails()
+def main_menu():
+    print('What would you like to do today? Enter the number that corresponds to what you want to do from the list.\n1: Get user details\n2: Get app details\n3: Get group details')
+    choice = input('Make your selection: ')
+    if choice == '1':
+        get_user_details()
+    elif choice == '2':
+        get_app_details()
+    elif choice =='3':
+        get_group_details()
     else:
-        print("Sorry, that choice is not valid! Try again.")
-        mainMenu()
+        print('Sorry, that choice is not valid! Try again.')
+        main_menu()
 
-mainMenu()
+main_menu()
